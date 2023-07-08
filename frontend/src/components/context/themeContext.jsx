217ae2d76cebfff78456e896bcbcfd31da9faa44
Rohit Kharche <111701144+rohitrokz29@users.noreply.github.com>
2023-07-08 18:31:52 +0530

    report page updated

--- frontend/src/components/context/themeContext.jsx
+++ frontend/src/components/context/themeContext.jsx
@@ -5,11 +5,9 @@ export const ThemeContext = createContext();
 export const ThemeState = ({ children}) => {
     const [theme, setTheme] = useState('light');
 
-
     const changeTheme=()=>{
         setTheme(theme=>theme==='light'?'dark':'light');
     }
-
     return (
         <ThemeContext.Provider value={{
             theme,
--- frontend/src/components/pages/Report/Report.jsx
+++ frontend/src/components/pages/Report/Report.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useSyncExternalStore } from 'react';
 import './report.css';
 import { useUserContext } from '../../hooks/useUserContext'
 import { useThemeContext } from '../../hooks/useThemeContext';
+import API from '../../api/api';
 
 const Report = () => {
     const { user } = useUserContext();
     const { theme } = useThemeContext();
+    const [reportData, setReportData] = useState({username:user?.username,email:user?.email,issue_type:"",issue:""})
+   const [confirm, setConfirm] = useState(true);
+   
+    const handleChange=(e)=>{
+    setReportData({...reportData,[e.target.name]:e.target.value})
+   }
+    const handleReport = (e) => {
+        e.preventDefault();
+        console.log({reportData,confirm});
+
+        API.post('/api/report')
+    }
     return (
-        <form className={`report-form bg-${theme}er`}>
+        <form className={`report-form bg-${theme}er`} onSubmit={handleReport} >
 
             <div className={`report-side bg-${theme}`}>
                 <h2 className={`dark-text-${theme}`}>Report the problem to us</h2>
@@ -15,17 +28,17 @@ const Report = () => {
             </div>
             <div className={`form bg-${theme}`}>
                 <div className="report-input">
-                    <label htmlFor="username" className={`dark-text-${theme}`}>Username:</label>
+                    <label htmlFor="username" className={`mid-text dark-text-${theme}`}>Username:</label>
                     <input type="text" name='username' id='username' value={user?.username || 'rrohiut'} disabled />
                 </div>
                 <div className="report-input">
-                    <label htmlFor="email" className={`dark-text-${theme}`}>Email:</label>
+                    <label htmlFor="email" className={`mid-text  dark-text-${theme}`}>Email:</label>
                     <input type="email" name='email' id='email' value={user?.email || 'erjnk4,'} disabled />
                 </div>
                 <div className="report-input">
-                    <label htmlFor="issue_type" className={`dark-text-${theme}`}>Issue Type:</label>
-                    <select name="issur_type" id="issue_type">
-                        <option value={null}>Choose Issue Type</option>
+                    <label htmlFor="issue_type" className={`mid-text dark-text-${theme}`}>Issue Type:</label>
+                    <select name="issue_type" id="issue_type" value={reportData.issue_type} onChange={handleChange}>
+                        <option value="null">Choose Issue Type</option>
                         <option value="report_user">Report a user</option>
                         <option value="journal">Any Problem with Journal</option>
                         <option value="trackers">Tracking Graphs</option>
@@ -34,14 +47,14 @@ const Report = () => {
                     </select>
                 </div>
                 <div className="report-input">
-                    <label htmlFor="issue" className={`dark-text-${theme}`}>Issue:</label>
-                    <textarea type="text" name="issue" id="issue" />
+                    <label htmlFor="issue" className={`mid-text dark-text-${theme}`}>Issue Description:</label>
+                    <textarea type="text" name="issue" id="issue"  onChange={handleChange} />
                 </div>
                 <div className="report-check">
-                    <input type="checkbox" name="confirm" id="confirm" />
-                    <label htmlFor="confirm" className={`dark-text-${theme}`}>By submitting this report you take the responsiblity of provided information</label>
+                    <input type="checkbox" name="confirm" id="confirm"  defaultChecked  onChange={(e)=>setConfirm(confirm=>!confirm)}/>
+                    <label htmlFor="confirm" className={`mid-text dark-text-${theme}`} >By submitting this report you take the responsiblity of provided information</label>
                 </div>
-                <div className="report-input-button">
+                <div className="report-input-button ">
                     <button className="add-button" type='submit'>Report</button>
                 </div>
 

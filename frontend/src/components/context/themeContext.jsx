ad7dbc44472b3bfa71971c2f1afcfd8777655e3a
Rohit Kharche <111701144+rohitrokz29@users.noreply.github.com>
2023-07-08 18:31:09 +0530

    added styles to report page

--- frontend/src/components/context/themeContext.jsx
+++ frontend/src/components/context/themeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeState = ({ children}) => {
-    const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState('light');
 
 
     const changeTheme=()=>{
--- frontend/src/components/pages/Report/Report.jsx
+++ frontend/src/components/pages/Report/Report.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './report.css';
 import { useUserContext } from '../../hooks/useUserContext'
 import { useThemeContext } from '../../hooks/useThemeContext';
 
@@ -6,28 +7,45 @@ const Report = () => {
     const { user } = useUserContext();
     const { theme } = useThemeContext();
     return (
-        <form className='report-form'>
-            <div className="report-input">
-                <label htmlFor="username">Username:</label>
-                <input type="text" name='username' id='username' value={user?.username} disabled />
-            </div>
-            <div className="report-input">
-                <label htmlFor="email" >Email:</label>
-                <input type="text" name='email' id='email' value={user?.email} disabled />
-            </div>
-            <div className="report-input">
-                <label htmlFor="issue_type">Issue Type:</label>
-                <input type="text" name="issue_type" id="issue_type" />
-            </div>
-            <div className="report-input">
-                <label htmlFor="issue">Issue:</label>
-                <textarea type="text" name="issue" id="issue" />
-            </div>
-            <div className="report-input">
-                <input type="checkbox" name="confirm" id="confirm" />
-                <label htmlFor="confirm">By submitting this report you take the responsiblity of provided information</label>
-            </div>
+        <form className={`report-form bg-${theme}er`}>
 
+            <div className={`report-side bg-${theme}`}>
+                <h2 className={`dark-text-${theme}`}>Report the problem to us</h2>
+                <h4 className={`light-text-${theme}`}>We will try our best to take your report into consideration.</h4>
+            </div>
+            <div className={`form bg-${theme}`}>
+                <div className="report-input">
+                    <label htmlFor="username" className={`dark-text-${theme}`}>Username:</label>
+                    <input type="text" name='username' id='username' value={user?.username || 'rrohiut'} disabled />
+                </div>
+                <div className="report-input">
+                    <label htmlFor="email" className={`dark-text-${theme}`}>Email:</label>
+                    <input type="email" name='email' id='email' value={user?.email || 'erjnk4,'} disabled />
+                </div>
+                <div className="report-input">
+                    <label htmlFor="issue_type" className={`dark-text-${theme}`}>Issue Type:</label>
+                    <select name="issur_type" id="issue_type">
+                        <option value={null}>Choose Issue Type</option>
+                        <option value="report_user">Report a user</option>
+                        <option value="journal">Any Problem with Journal</option>
+                        <option value="trackers">Tracking Graphs</option>
+                        <option value="Others">Other Issue</option>
+
+                    </select>
+                </div>
+                <div className="report-input">
+                    <label htmlFor="issue" className={`dark-text-${theme}`}>Issue:</label>
+                    <textarea type="text" name="issue" id="issue" />
+                </div>
+                <div className="report-check">
+                    <input type="checkbox" name="confirm" id="confirm" />
+                    <label htmlFor="confirm" className={`dark-text-${theme}`}>By submitting this report you take the responsiblity of provided information</label>
+                </div>
+                <div className="report-input-button">
+                    <button className="add-button" type='submit'>Report</button>
+                </div>
+
+            </div>
         </form>
     )
 }
